56aad4f818ddcea9e0201265f8ae1873294e4eac
William Huang <whuang37@ucla.edu>
2024-05-10 01:20:44 -0700

    emptied pages branch

--- wheelpose_unity_env/Assets/Scripts/SpriteRandomizer/SpriteRandomizerTag.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UnityEngine.Perception.Randomization.Randomizers.Tags
-{
-    /// <summary>
-    /// Used in conjunction with a SpriteRandomizer to vary the sprite of GameObjects
-    /// </summary>
-    [RequireComponent(typeof(SpriteRenderer))]
-    [AddComponentMenu("Perception/RandomizerTags/Sprite Randomizer Tag")]
-    public class SpriteRandomizerTag : RandomizerTag {}
-}
